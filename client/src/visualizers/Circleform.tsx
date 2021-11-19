a2ffa772fcea5f0c1073a821caebff081a98024c
cctina516 <christinayan1019@gmail.com>
2021-11-18 17:28:21 -0800

    udpate circleform

--- client/src/visualizers/Circleform.tsx
+++ client/src/visualizers/Circleform.tsx
@@ -4,6 +4,21 @@ import * as Tone from "tone";
 
 // project imports
 import { Visualizer } from "../Visualizers";
+//let p6: P5;
+let particles: Particle[] = [];
+
+class Particle {
+  public pos: any;
+  constructor() {
+    this.pos = P5.Vector.random2D().mult(250);
+  }
+
+  show(p6: P5) {
+    p6.noStroke();
+    p6.fill(255);
+    p6.ellipse(this.pos.x, this.pos.y, 4);
+  }
+}
 
 export const CircleformVisualizer = new Visualizer(
   "Circleform",
@@ -18,15 +33,38 @@ export const CircleformVisualizer = new Visualizer(
     p5.stroke(255, 255, 255, 255);
     p5.noFill();
 
+    p5.translate(width / 2, height / 2);
+
+    p5.angleMode("degrees");
+
     const values = analyzer.getValue();
     p5.beginShape();
-    for (let i = 0; i < values.length; i++) {
+    for (let i = 0; i <= 180; i++) {
       const amplitude = values[i] as number;
-      const x = p5.map(i, 0, values.length - 1, 0, width);
-      const y = height / 2 + amplitude * height;
+      const r = p5.map(amplitude, -1, 1, 100, 300);
+      const x = r * p5.sin(i);
+      const y = r * p5.cos(i);
       // Place vertex
       p5.vertex(x, y);
     }
     p5.endShape();
+    // the other half of the circle
+    p5.beginShape();
+    for (let i = 0; i <= 180; i++) {
+      const amplitude = values[i] as number;
+      const r = p5.map(amplitude, -1, 1, 100, 300);
+      const x = r * -p5.sin(i);
+      const y = r * p5.cos(i);
+      // Place vertex
+      p5.vertex(x, y);
+    }
+    p5.endShape();
+
+    let p = new Particle();
+    particles.push(p);
+
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].show(p5);
+    }
   }
 );
