f1d41722edde50291b47d44b2d4aaf8ebc254ca5
cctina516 <christinayan1019@gmail.com>
2021-12-02 17:28:26 -0800

    add circleForm to a my 2nd branch

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -1,11 +1,11 @@
 // 3rd party
-import { List, Map } from "immutable";
+import { List, Map } from 'immutable';
 
 // project dependencies
-import { PianoInstrument } from "./instruments/Piano";
-import { WaveformVisualizer } from "./visualizers/Waveform";
-import { CircleformVisualizer } from "./visualizers/Circleform";
-import { SaxophoneInstrument } from "./instruments/Saxophone";
+import { PianoInstrument } from './instruments/Piano';
+import { WaveformVisualizer } from './visualizers/Waveform';
+import { CircleformVisualizer } from './visualizers/cctina516-Visualizer';
+import { XylophoneInstrument } from './instruments/cctina516-Instrument';
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -19,7 +19,7 @@ import { SaxophoneInstrument } from "./instruments/Saxophone";
  */
 export type AppState = Map<string, any>;
 
-const instruments = List([PianoInstrument, SaxophoneInstrument]);
+const instruments = List([PianoInstrument, XylophoneInstrument]);
 const visualizers = List([WaveformVisualizer, CircleformVisualizer]);
 export const defaultState: AppState = Map<string, any>({
   instruments,
